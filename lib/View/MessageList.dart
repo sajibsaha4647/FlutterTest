84f09cb06a088053922423c1f0b5625bae96d429
sajibsaha4647 <sksaha4648@gmail.com>
2022-08-25 23:01:42 +0600

    updated

---
+++ lib/View/MessageList.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MessageList extends StatefulWidget {
+  const MessageList({Key? key}) : super(key: key);
+
+  @override
+  State<MessageList> createState() => _MessageListState();
+}
+
+class _MessageListState extends State<MessageList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
